2f2b549346a5a51e70d7f5a6c3882d6d735cbaae
sammy0329 <38906420+sammy0329@users.noreply.github.com>
2025-04-11 15:12:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.11 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,60 +1,56 @@
 import java.util.*;
 
-class Solution {
-    
-    // Point 객체
-    static class Point {
-        int r;
-        int c;
-        public Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
+class Point {
+    int r;
+    int c;
+    public Point(int r, int c) {
+        this.r = r;
+        this.c = c;
     }
+}
+
+class Solution {
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
     
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
         boolean[][] isVisited = new boolean[n][m];
         
-        // 방향 배열 (상, 하, 좌, 우)
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-        
-        Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(0, 0));
+        Queue<Point> q = new ArrayDeque<>();
         isVisited[0][0] = true;
+        q.offer(new Point(0, 0));
         
-        int answer = 1; // 시작 지점도 포함되므로 거리 1로 초기화
+        int depth = 1;
         
-        while (!q.isEmpty()) {
-            int qSize = q.size();
-            for (int i = 0; i < qSize; i++) {
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i=0; i<size; i++) {
                 Point current = q.poll();
                 
-                // 목표 지점에 도달하면 반환
-                if (current.r == n - 1 && current.c == m - 1) {
-                    return answer;
+                if(current.r == n-1 && current.c == m-1) {
+                    return depth;
                 }
                 
-                // 4방향 탐색
-                for (int d = 0; d < 4; d++) {
+                for(int d=0; d<4; d++) {
                     int nr = current.r + dr[d];
                     int nc = current.c + dc[d];
                     
-                    // 범위 밖이거나 벽이거나 이미 방문한 경우 스킵
-                    if (nr < 0 || nr >= n || nc < 0 || nc >= m || maps[nr][nc] == 0 || isVisited[nr][nc]) {
+                    if(nr < 0 || nc < 0 || nr >= n || nc >= m || 
+                       maps[nr][nc] == 0 || isVisited[nr][nc]) {
                         continue;
                     }
                     
-                    // 큐에 넣을 때 방문 처리
-                    q.offer(new Point(nr, nc));
                     isVisited[nr][nc] = true;
+                    q.offer(new Point(nr, nc));
                 }
             }
-            answer++;
+            
+            depth++;
         }
         
-        return -1; // 목표 지점에 도달할 수 없는 경우
+        return -1;
     }
 }
