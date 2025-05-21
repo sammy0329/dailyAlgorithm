d4149abd3081c41cff9cb6593f9ad8169029f75d
sammy0329 <38906420+sammy0329@users.noreply.github.com>
2025-05-21 11:36:03 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 2.84 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,36 +1,6 @@
-import java.util.*;
-
-public class Solution {
-    public int solution(int[] mats, String[][] park) {        
-        int n = park.length;
-        int m = park[0].length;
-
-        Arrays.sort(mats);  // 오름차순 정렬 후 큰 돗자리부터 확인
-        
-        for (int k = mats.length - 1; k >= 0; k--) {
-            int matSize = mats[k];
-
-            for (int r = 0; r <= n - matSize; r++) {
-                for (int c = 0; c <= m - matSize; c++) {
-                    if (canPlace(park, r, c, matSize)) {
-                        return matSize;
-                    }
-                }
-            }
-        }
-
-        return -1;
-    }
-
-    // 해당 돗자리를 깔 수 있는지 확인 메서드 
-    private boolean canPlace(String[][] park, int startR, int startC, int size) {
-        for (int i = startR; i < startR + size; i++) {
-            for (int j = startC; j < startC + size; j++) {
-                if (!park[i][j].equals("-1")) {
-                    return false;
-                }
-            }
-        }
-        return true;
+class Solution {
+    public int solution(int[] mats, String[][] park) {
+        int answer = 0;
+        return answer;
     }
 }
