1b3e6b016e16bf15ee82407092c25ba07e3386e1
sammy0329 <38906420+sammy0329@users.noreply.github.com>
2025-06-13 13:03:22 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.72 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -2,38 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        int n = targets.length;
-        int[] answer = new int[n];
-        Map<Character, Integer> wordMap = new HashMap<>();
-        
-        for (String k : keymap) {
-            for (int i = 0; i < k.length(); i++) {
-                char c = k.charAt(i);
-                int idx = i + 1;
-                
-                if (wordMap.containsKey(c)) { // 이미 존재하면 더 작은 값으로 갱신
-                    wordMap.put(c, Math.min(wordMap.get(c), idx));
-                } else {
-                    wordMap.put(c, idx);
-                }
+        int[] answer = new int[targets.length];
+        int[] charMap = new int[26]; // 'A' ~ 'Z'만 사용
+        Arrays.fill(charMap, Integer.MAX_VALUE);
+
+        for (String key : keymap) {
+            for (int i = 0; i < key.length(); i++) {
+                char c = key.charAt(i);
+                int idx = c - 'A';
+                charMap[idx] = Math.min(charMap[idx], i + 1);
             }
         }
-        
-        A: for(int i=0; i<n; i++){
-            String t = targets[i];
-            int result = 0;
-            
-            for(int j=0; j<t.length(); j++){
-                char c = t.charAt(j);
-                if(!wordMap.containsKey(c)){
-                    answer[i] = -1;
-                    continue A;
+
+        for (int i = 0; i < targets.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < targets[i].length(); j++) {
+                char c = targets[i].charAt(j);
+                int idx = c - 'A';
+                if (idx < 0 || idx >= 26 || charMap[idx] == Integer.MAX_VALUE) {
+                    sum = -1;
+                    break;
                 }
-                result += wordMap.get(c);
+                sum += charMap[idx];
             }
-            answer[i]=result;
+            answer[i] = sum;
         }
-        
+
         return answer;
     }
 }
