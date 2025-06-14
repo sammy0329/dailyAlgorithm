b921bd7a7e3e255c28a47c5654b3853a56924748
sammy0329 <38906420+sammy0329@users.noreply.github.com>
2025-06-14 10:36:30 +0900

    [level 2] Title: 타겟 넘버, Time: 7.56 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,38 +1,20 @@
-import java.util.*;
-
 class Solution {
-    static char[] ch = {'+','-'};
-    static char[] picked;
     static int answer;
     
-    public void dfs(int[] numbers, int n, int target, int cnt){
-        if(cnt == n){
-            int result = 0;
-            for(int i=0; i<n; i++){
-                if(picked[i] == '+'){
-                    result += numbers[i];
-                }else if(picked[i] == '-'){
-                    result -= numbers[i];
-                }
-            }
-            if(result == target){
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, numbers.length, 0, 0, target);
+        return answer;
+    }
+    
+    public void dfs(int[] numbers, int n, int cnt, int sum, int target){
+        if(cnt==n){
+            if(sum == target){
                 answer++;
             }
             return;
         }
         
-        // 현재 위치(cnt)의 숫자에 대해서만 + 또는 - 연산자 선택
-        picked[cnt] = '+';
-        dfs(numbers, n, target, cnt+1);
-        
-        picked[cnt] = '-';
-        dfs(numbers, n, target, cnt+1);
-    }
-    
-    public int solution(int[] numbers, int target) {
-        int n = numbers.length;
-        picked = new char[n];
-        dfs(numbers, n, target, 0);
-        return answer;
+        dfs(numbers, n, cnt+1, sum+numbers[cnt], target);
+        dfs(numbers, n, cnt+1, sum-numbers[cnt], target);
     }
 }
