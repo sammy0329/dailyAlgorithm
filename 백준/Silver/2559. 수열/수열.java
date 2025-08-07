59f2dc625713a05a9360e91a3e67260269f91e0f
sammy0329 <38906420+sammy0329@users.noreply.github.com>
2025-08-07 23:50:57 +0900

    [Silver III] Title: 수열, Time: 168 ms, Memory: 22596 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int max = Integer.MIN_VALUE;
+
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
@@ -19,13 +19,21 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i <= n - k; i++) {
-            int a = 0;
-            for (int j = i; j < i + k; j++) {
-                a += arr[j];
-            }
-            max = Math.max(max, a);
+        // 처음 윈도우 k개의 합 구하기
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
         }
-        System.out.println(max);
+        int max = sum;
+
+        // 슬라이딩 윈도우 적용하여 최대값 갱신
+        for (int i = k; i < n; i++) {
+            sum = sum - arr[i - k] + arr[i];
+            if (sum > max) {
+                max = sum;
+            }
+        }
+
+        System.out.println(max);  // 결과 출력
     }
 }
