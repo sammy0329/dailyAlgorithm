21775ecee2e0f7718cf6d9743e2433ea2124e0db
sammy0329 <38906420+sammy0329@users.noreply.github.com>
2025-11-26 16:32:46 +0900

    [level 2] Title: 프로세스, Time: 0.95 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,32 +1,27 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        HashSet<Integer> set = new HashSet<>();
-        int answer = 0;
-
-        // PriorityQueue에 priorities 값 넣기
-        for (int i = 0; i < priorities.length; i++) {
-            pq.add(priorities[i]);
+        int count = 0;
+        
+        for (int priority : priorities) {
+            pq.add(priority);
         }
 
-        A:
         while (!pq.isEmpty()) {
-            for (int i = 0; i < priorities.length; i++) {
-                if (!set.contains(i) && priorities[i] == pq.peek()) {
-                    pq.poll();
-                    set.add(i);
-                    answer++;
+           for(int i = 0; i < priorities.length; i++) {
+               if(priorities[i] == pq.peek()) {
+                   pq.poll();
+                   count++;
 
-                    if (i == location) {
-                        break A;
-                    }
-                }
-            }
+                   if(i == location) {
+                       return count;
+                   }
+               }
+           }
         }
         
-        return answer;
+        return count;
     }
 }
