a524cb9ab765b2f0e5ad3f8fb9eac6b704ac0c4c
sammy0329 <38906420+sammy0329@users.noreply.github.com>
2026-03-03 20:05:38 +0900

    [level 3] Title: 네트워크, Time: 0.63 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,36 +1,27 @@
-import java.util.*;
-
 class Solution {
-    static boolean[] isVisited;
-    
-    public void bfs(int[][] computers, int n, int start){
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        isVisited[start] = true;
-        
-        while(!q.isEmpty()){
-            int current = q.poll();
-            
-            for(int i=0; i<n; i++){
-                if(computers[current][i] == 1 && !isVisited[i]){
-                    isVisited[i] = true;
-                    q.offer(i);
-                }
-            }
-        }
-    }
-    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        isVisited = new boolean[n];
-        
-        for(int i=0; i<n; i++){
-            if(!isVisited[i]){
-                bfs(computers, n, i);
-                answer++;
+        boolean[] visited = new boolean[n]; // 방문 여부 체크
+
+        for (int i = 0; i < n; i++) {
+            // 아직 방문하지 않은 컴퓨터라면 새로운 네트워크의 시작점
+            if (!visited[i]) {
+                dfs(i, n, computers, visited);
+                answer++; // DFS가 끝나면 네트워크 한 개를 다 찾은 것
             }
         }
-        
+
         return answer;
+    }
+
+    private void dfs(int i, int n, int[][] computers, boolean[] visited) {
+        visited[i] = true; // 현재 컴퓨터 방문 처리
+
+        for (int j = 0; j < n; j++) {
+            // 1. 자기 자신이 아니고 2. 연결되어 있으며 3. 아직 방문하지 않았다면
+            if (i != j && computers[i][j] == 1 && !visited[j]) {
+                dfs(j, n, computers, visited);
+            }
+        }
     }
 }
