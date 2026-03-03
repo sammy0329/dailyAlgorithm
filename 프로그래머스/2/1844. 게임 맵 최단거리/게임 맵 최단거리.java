5f7bab97bdfdbcabc1faec4b4a0cf34eb3d85ff0
sammy0329 <38906420+sammy0329@users.noreply.github.com>
2026-03-04 08:47:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.54 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,56 +1,55 @@
 import java.util.*;
 
-class Point {
-    int r;
-    int c;
-    public Point(int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
-}
-
 class Solution {
-    int[] dr = {-1, 1, 0, 0};
-    int[] dc = {0, 0, -1, 1};
-    
+    static class Point {
+        int r, c;
+        Point(int r, int c) {
+            this.r = r; this.c = c;
+        }
+    }
+
+    static int[] dr = {1, -1, 0, 0};
+    static int[] dc = {0, 0, 1, -1};
+
     public int solution(int[][] maps) {
+        return bfs(maps);
+    }
+
+    private int bfs(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-        boolean[][] isVisited = new boolean[n][m];
-        
+
         Queue<Point> q = new ArrayDeque<>();
-        isVisited[0][0] = true;
+        boolean[][] visited = new boolean[n][m];
+
         q.offer(new Point(0, 0));
-        
+        visited[0][0] = true;
+
         int depth = 1;
-        
-        while(!q.isEmpty()) {
-            int size = q.size();
-            
-            for(int i=0; i<size; i++) {
-                Point current = q.poll();
-                
-                if(current.r == n-1 && current.c == m-1) {
+
+        while (!q.isEmpty()) {
+            int cnt = q.size();
+
+            for (int i = 0; i < cnt; i++) {
+                Point p = q.poll();
+
+                if (p.r == n - 1 && p.c == m - 1){
                     return depth;
                 }
                 
-                for(int d=0; d<4; d++) {
-                    int nr = current.r + dr[d];
-                    int nc = current.c + dc[d];
-                    
-                    if(nr < 0 || nc < 0 || nr >= n || nc >= m || 
-                       maps[nr][nc] == 0 || isVisited[nr][nc]) {
-                        continue;
-                    }
-                    
-                    isVisited[nr][nc] = true;
+                for (int d = 0; d < 4; d++) {
+                    int nr = p.r + dr[d];
+                    int nc = p.c + dc[d];
+
+                    if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+                    if (maps[nr][nc] == 0 || visited[nr][nc]) continue;
+
+                    visited[nr][nc] = true;
                     q.offer(new Point(nr, nc));
                 }
             }
-            
             depth++;
         }
-        
         return -1;
     }
 }
